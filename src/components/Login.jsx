321f087d128d9de6fa7296fca5224e4367864813
perezmanfredo959@gmail.com <perezmanfredo959@gmail.com>
2024-10-25 18:49:00 -0600

    Redirect

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,9 @@ export default function Login() {
 
     try {
       setLoading(true)
-      const { error } = await supabase.auth.signInWithOtp({ email })
+      const { error } = await supabase.auth.signInWithOtp({ email }, {
+        redirectTo: 'https://dashboardadminsancris-1.onrender.com/',
+      })
       if (error) throw error
       alert('Revise su correo electrónico para ver el enlace de inicio de sesión!')
     } catch (error) {
